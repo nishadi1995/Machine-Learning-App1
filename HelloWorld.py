856d1147838086777be0c115af2c5f03da6a0f4e
nishadi1995 <nishadinuwa1995@gmail.com>
2019-02-18 11:08:42 +0530

    basic implementation

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+from sklearn import tree
+features = [[140,1],[130,1],[150,0],[170,0],[145,1]]
+labels=[0,0,1,1,0]
+
+clf = tree.DecisionTreeClassifier()
+clf=clf.fit(features,labels)
+print clf.predict([[150,0]])
+print clf.predict([[146,0]])
+
+
